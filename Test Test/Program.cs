1d480bd18aa79844e2455e7dabe03dec7d8b5b96
Thomas <thomas.lofink1@gmail.com>
2023-09-11 13:31:12 +0200

    kpokpkpopo

--- Test Test/Program.cs
+++ Test Test/Program.cs
@@ -9,7 +9,12 @@ namespace Test_Test
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
+                
         }
     }
+    //int zahl1 = 8;
+    //int zahl2 = 2;
+    //int erg = zahl1 + zahl2;
+    //Console.Writeline(zahl1 "+" zahl2 "=" erg); 
 }
